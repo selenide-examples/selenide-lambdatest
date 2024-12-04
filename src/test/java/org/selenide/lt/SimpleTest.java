1daa66c722e9570a16b624fa24b840384e4f2a25
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-12-04 09:20:37 +0200

    add more checks to Selenide users test

--- src/test/java/org/selenide/lt/SimpleTest.java
+++ src/test/java/org/selenide/lt/SimpleTest.java
@@ -1,12 +1,14 @@
 package org.selenide.lt;
 
+import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.CollectionCondition.sizeLessThan;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
-
-import org.junit.jupiter.api.Test;
 
 public class SimpleTest {
   @Test
@@ -15,6 +17,12 @@ public class SimpleTest {
     $(".main-menu-pages").find(byText("Users")).click();
 
     $$("#user-tags .tag").shouldHave(sizeGreaterThan(8));
-    $$("#selenide-users .user").shouldHave(sizeGreaterThan(20));
+    $$("#selenide-users .user:not(.hidden)").shouldHave(sizeGreaterThan(80));
+
+    $("#user-tags").find(byTagAndText("a", "ukraine")).click();
+    $$("#selenide-users .user:not(.hidden)").shouldHave(sizeLessThan(80));
+
+    $("#user-tags").find(byTagAndText("a", "healthcare")).click();
+    $$("#selenide-users .user:not(.hidden)").shouldHave(sizeLessThan(20));
   }
 }
